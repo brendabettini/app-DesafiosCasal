214d8720a32a80c2a2e0c5e8ca7db8034f7c872a
Hiago VR <hiagovribeiro5@gmail.com>
2024-05-14 21:32:15 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Inicial.kt
@@ -0,0 +1,49 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import crud_banco.R
+import crud_banco.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class Inicial : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                setContentView(R.layout.activity_main)
+                binding = ActivitySignInBinding.inflate(layoutInflater)
+                setContentView(binding.root)
+
+                firebaseAuth = FirebaseAuth.getInstance()
+
+                binding.textView.setOnClickListener {
+                        val intent = Intent(this, SignUpActivity::class.java)
+                        startActivity(intent)
+                }
+
+                binding.button.setOnClickListener{
+
+                        val email = binding.emailEt.text.toString()
+                        val pass = binding.passET.text.toString()
+
+
+                        if (email.isNotEmpty() && pass.isNotEmpty()) {
+                                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                                        if (it.isSuccessful) {
+                                                val intent = Intent(this, MainActivity::class.java)
+                                                startActivity(intent)
+                                        } else {
+                                                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                                        }
+                                }
+                        } else {
+                                Toast.makeText(this, "Campos vazios não são permitidos!!", Toast.LENGTH_SHORT).show()
+
+                        }
+                }
+        }
+}
+        }
+}
